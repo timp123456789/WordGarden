6f2096af2ea4147fbf41cb61c7333683e2f791f9
Timohty Petrik <timmypetrik123456789@gmail.com>
2025-09-17 14:14:22 -0400

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Timothy Petrik on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
